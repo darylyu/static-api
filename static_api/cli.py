fb8650e064fbb7006b46b477dba64873ab8ad9dd
D. Yu <dyu@fastmail.com>
2016-07-31 20:50:53 +0800

    Working basic(super basic!) version.

--- static_api/cli.py
+++ static_api/cli.py
@@ -1,13 +1,37 @@
 # -*- coding: utf-8 -*-
 
 import click
+import json
+
+from flask import Flask, jsonify
+
+
+api = Flask(__name__)
+FILE_PATH = '.'
+
+
+def open_data_file(file_path):
+    with open(file_path) as fp:
+        r = json.load(fp)
+        return r
+
+
+@api.route('/', defaults={'path': ''})
+@api.route('/<path:path>')
+def get_dir(path):
+    path = path.rstrip('/')
+    r = open_data_file(FILE_PATH + '/' + path + '.json')
+    return jsonify(r)
+
 
 @click.command()
-def main(args=None):
+@click.argument('static_path')
+def main(static_path):
     """Console script for static_api"""
-    click.echo("Replace this message by putting your code into "
-                "static_api.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    click.echo(static_path)
+    global FILE_PATH
+    FILE_PATH = static_path
+    api.run()
 
 
 if __name__ == "__main__":
--- static_api/static_api.py
+++ static_api/static_api.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+from flask import Flask
+api = Flask(__name__)
+
+@api.route('/', defaults={'url': ''})
+@app.route('/<url:url>')
+def get_dir(url):
+    return url
